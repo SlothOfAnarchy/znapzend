dc9312c487d2f85f04d1ef4b2b874bdae4fd2f00
Michel Weitbrecht <>
2020-02-13 22:45:56 +0100

    remove trace

--- lib/ZnapZend.pm
+++ lib/ZnapZend.pm
@@ -664,7 +664,7 @@ my $sendWorker = sub {
             my ($fc, $err) = @_;
 
             $self->zLog->warn('send/receive for ' . $backupSet->{src}
-                . ' failed: ' . $err . '(time:' . $timeStamp . ', backup set: ' . $backupSet .') Trace: ' . $err->trace) if $err;
+                . ' failed: ' . $err . '(time:' . $timeStamp . ', backup set: ' . $backupSet .') ') if $err;
 
             $self->zLog->debug('send/receive worker for ' . $backupSet->{src}
                 . " done ($backupSet->{send_pid})");
